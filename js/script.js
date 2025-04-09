c8c5d7dcc9a25b41b661ff96932c11147aeb0f58
Andrei Novikau <andrei.novikau92@gmail.com>
2025-04-09 06:58:54 -0500

    add: rememberMyFilms function and detectPersonalLevel function

--- js/script.js
+++ js/script.js
@@ -21,27 +21,35 @@ const personalMovieDB = {
     'privat': false
 };
 
-for(let i = 0; i < 2; i++) {
-    const a = prompt('One of the last movies you watched?', '');
-    const b = prompt('How much would you rate it?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms () {
+    for(let i = 0; i < 2; i++) {
+        const a = prompt('One of the last movies you watched?', '');
+        const b = prompt('How much would you rate it?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-if(personalMovieDB.count < 10) {
-    console.log('Watched quite a few films');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('You are a classic spectator');
-} else if (personalMovieDB.count >= 30) {
-    console.log('You are a movie buff!');
-} else {
-    console.log('Error');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(personalMovieDB.count < 10) {
+        console.log('Watched quite a few films');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You are a classic spectator');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('You are a movie buff!');
+    } else {
+        console.log('Error');
+    }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
